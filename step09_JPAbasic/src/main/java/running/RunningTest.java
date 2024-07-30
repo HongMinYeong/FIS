fda3eca9a1f88e90b4bc5bf1f1e1fb02a1c4a65e
hongminyeong <minyung1240@daum.net>
2024-07-30 12:04:47 +0900

    STUDY: row create, find, update 공부

--- step09_JPAbasic/src/main/java/running/RunningTest.java
+++ step09_JPAbasic/src/main/java/running/RunningTest.java
@@ -17,11 +17,14 @@ public class RunningTest {
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		
-		em.persist(new Customer("id1", "연아", 50));
+//		em.persist(new Customer("id1", "연아", 50)); // no가 1로 저장되어 있음 
 		
 		Customer c1 = em.find(Customer.class, 1);
-		System.out.println(c1);
-		//tx.commit(); 
+		System.out.println(c1); 
+		// Customer(no=1, id=id1, name=연아, age=50)
+		
+		c1.setName("유다연"); // update
+		tx.commit(); 
 	
 		em.close();
 	}
