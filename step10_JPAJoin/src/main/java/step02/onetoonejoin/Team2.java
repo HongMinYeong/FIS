d7d89d78ac2dce3fc22cb6620a8bda29f1bc045a
hongminyeong <minyung1240@daum.net>
2024-07-31 16:08:02 +0900

    STUDY:sequence 오류, FK 오류로 인한 데이터 무결성 문제 해결

--- step10_JPAJoin/src/main/java/step02/onetoonejoin/Team2.java
+++ step10_JPAJoin/src/main/java/step02/onetoonejoin/Team2.java
@@ -22,12 +22,26 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+//create sequence member_seq_id start with 1 increment by 50
 
+/*@SequenceGenerator
+ * 	- 해당 entity 에 적용되는 sequence 를 생성 및 적용 의미
+ * 	- name = "team_seq" 
+ * 		: sequence 를 entity 상에서 활용하고자 하는 설정 이름 부여
+ * 		: @GenerateValue 라는 애노테이션의 generator 속성이 참조하는 이름 설정 
+ * 	- sequenceName = "team_seq_id" : db 에 생성되는 sequence명
+ * 	- allocationSize = 50
+ * 		: 한번에 50개 할당(생성)해 놓겠다는 의미
+ * 		: insert 시에 sequence 생성이 아닌 사전에 50개 만들어 놓고 배정
+ * 			-> 실행 속도 고려 
+ * 
+ */
 @SequenceGenerator(name = "team_seq", sequenceName = "team_seq_id",
 				   initialValue = 1, allocationSize = 50)
-@Entity
+//@Entity
 public class Team2 {
-
+	// strategy = GenerationType.SEQUENCE :oracle seq
+	// strategy = GenerationType.IDENTITY : mysql auto_increment
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "team_seq")
 	@Column(name = "team_id")
--- step10_JPAJoin/src/main/java/step03/onetoonejoin/Member3.java
+++ step10_JPAJoin/src/main/java/step03/onetoonejoin/Member3.java
@@ -26,7 +26,7 @@ import lombok.ToString;
 
 @SequenceGenerator(name = "member3_seq", sequenceName = "member3_seq_id", 
 				   allocationSize = 50, initialValue = 1)
-//@Entity
+@Entity
 public class Member3 {
 
 	@Id
@@ -35,12 +35,24 @@ public class Member3 {
 	private long memberId;
 	
 	@NonNull
-	@Column(length = 20) //컬럼 사이즈 20byte
+	@Column(length = 20, nullable = false) //컬럼 사이즈 20byte
 	private String name;
+	/* ?
+	 * create table Member3 (
+	 * member_id number(19,0) not null,
+	 *  name varchar2(20) not null,
+	 *  team_id number(19,0), 
+	 *  primary key (member_id)
+	 *  );
+	 *  
+	 * 
+	 * alter table Member3 add constraint  foreign key (team_id) references Team3;
+	 */
 	
 	@NonNull
-	@OneToOne
+	@OneToOne // Memeber 하나는 Team 하나에 소속 
 	@JoinColumn(name="team_id")  //Team3의 pk 변수에 선언된 매핑된 컬럼명
 	private Team3 teamId;
+	// long 타입에서 Team3이라는 참고객체 
 	
 }
